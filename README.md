9f5b9628887e66c4a1034a9dabf940412632318b
Orion Ford <39802627+DesignisOrion@users.noreply.github.com>
2021-02-06 15:59:20 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Henry The Butler (A.I Virtual Assistant)
+
+### About 
+Henry is an A.I Virtual Assistant that can be used to automate task on the desktop. This is a startup project that is currently being updated daily. Enjoy.
+
+### Technologies Used:
+- Python 3 (language of program)
+- Speech Recognition Module
+- PyAudio Module
+- Pyttsx3 Module 
+
+# Author: 
+Orion F.
+www.DesignIsOrion.com
---
+++ apptalking.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr 
+import pyttsx3
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+engine.say('I am Henry The Butler')
+engine.say('How can I assist you Mr. Ford?')
+engine.runAndWait()
+
+
+try:
+    # recording from mic and recording
+    with sr.Microphone() as source:
+            
+        # Infoming user that Henry is listening.
+        print('Henry is listening...')
+        voice = listener.listen(source)
+        # functions for voice to text
+        command = listener.recognize_google(voice)
+            
+        # dictate if Henry is there or not.
+        # This will allow it to be called only when saying Banky
+        command = command.lower()
+        if 'Henry' in command:
+                print(command)
+except:
+    pass
+    
